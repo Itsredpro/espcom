3ab20069c98af4f3a3d6939709334deadda20b5a
RedDev <thomhmail@gmail.com>
2024-11-01 19:42:18 +0100

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,19 +1,29 @@
 #include "espcom_v2.h"
 
 void setup() {
+  // put your setup code here, to run once:
 
-  //start as client (make sure all callbacks are registered before starting otherwise unexpected behavior may occur)
+  
+  //Client example:
   espcom_set_mode(false); // host = false
-  espcom_init_wifi();
-  espcom_init_espnow();
 
-  espcom_auto_findgame([](game_struct){
-    //Do something when a game is found
+  //(make sure all callbacks are registered before starting otherwise unexpected behavior may occur)
+  espcom_init(); //initializes wifi and espnow capabilities.
+
+  espcom_auto_findgame([](game_struct){ // scan wifi channels 1-13 for game beacon frames
+    //Do something when a game is found..
     log_d("Found a game on channel: " + String(game_struct.channel));
   });
+
+  //Host example:
+  espcom_set_mode(true); // host = true
+  espcom_init();
+
+  espcom_sendPairingBeacon(JOINABLE); //send a single game pairing frame (on current wifi channel)
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  espcom_tick();
+  espcom_tick(); //important for processing sync tasks
 }
